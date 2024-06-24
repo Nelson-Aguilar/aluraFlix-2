b3ee13c6eeeeb1fcb11a7498bf1021496e359e83
Nelson-Aguilar <157563219+Nelson-Aguilar@users.noreply.github.com>
2024-06-23 21:10:54 -0600

    Add files via upload

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,13 @@
+import Hero from "../Hero";
+import { SectionCategory } from "../Categoria";
+
+function Main() {
+  return (
+    <main>
+      <Hero />
+      <SectionCategory />
+    </main>
+  );
+}
+
+export default Main;
